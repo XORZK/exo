bbe575440247e544973b70fb51ea1ae4956bbca2
xzk <2409hw@gmail.com>
2023-07-26 14:00:58 -0400

    barycentric coordinates

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 JS_LOC=./js/
+IMG_LOC=./img/
 IN=./src/
 OUT=./html/
 TEMPLATE=templates/template.html
@@ -8,6 +9,7 @@ TEMPLATE=templates/template.html
 mkdir "$OUT"
 rm -r "$OUT*"
 cp -r "$JS_LOC" "$OUT"
+cp -r "$IMG_LOC" "$OUT"
 
 for file in $(find "$IN" -type f); do
     file_name=$(basename "$file")
